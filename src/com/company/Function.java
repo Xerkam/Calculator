7838b8dd28e1e49b70d967624f70c89b2faf6fc0
Xerkam <peshevd@gmail.com>
2019-08-30 23:59:08 -0700

    Shunting-yard implementation

--- src/com/company/Function.java
+++ src/com/company/Function.java
@@ -1,9 +1,8 @@
 package com.company;
 
+import java.util.EmptyStackException;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Stack;
-import java.util.concurrent.ArrayBlockingQueue;
 
 public class Function {
 
@@ -25,7 +24,7 @@ public class Function {
         if (token.equals("x"))
             return true;
         try {
-            int x = Integer.parseInt(token);
+            double x = Double.parseDouble(token);
         } catch (NumberFormatException | NullPointerException nfe) {
             return false;
         }
@@ -59,7 +58,6 @@ public class Function {
             case "/":
             case "+":
             case "-":
-            case "(":
                 return true;
         }
         return false;
@@ -90,28 +88,65 @@ public class Function {
             if (isOperand(element)) {
                 e = i + 1;
 
-                //Checks to see if a found operand has more operands after it (A.K.A is this a multidigit number) as long
-                // index variable (i) is not looking at final character
-                while (e < equation.length() && isOperand(equation.substring(i, e + 1))) {
+                //Checks to see if a found operands has more digits after it or a decimal point (indicating a larger/longer
+                //number
+
+                while (e < equation.length() && (isOperand(equation.substring(e, e + 1)) || equation.substring(e, e + 1).equals("."))) {
                     e++;
                 }
 
                 output.addLast(equation.substring(i, e));
                 i = e - 1;// Set I to be the amount of characters skipped
 
-            } else if (isOperator(element))
-                operator.push(element);
+            }
 
-                //Checks if equation has a trig function within it, index variable (i) cant be more than equation.length() - 5
-                // because there are, at the least, 6 characters that a trig function needs to have. ex: sin(x)
+            //Checks if equation has a trig function within it, index variable (i) cant be more than equation.length() - 5
+            // because there are, at the least, 6 characters that a trig function needs to have. ex: sin(x)
 
             else if (i < equation.length() - 5 && isFunction(equation.substring(i, i + 3))) {
 
                 operator.push(equation.substring(i, i + 3));
-                i += 2; // Skip next to characters of trig function
+                i += 2; // Skip next two characters of trig function
+            }
+
+            else if (isOperator(element)) { //If element is an operator
+                while (!operator.isEmpty()
+                        && (isFunction(operator.peek()) //If the top of operator stack is a function
+                        || (precedance(operator.peek()) > precedance(element)) //or precedence of top operator is more than current
+                        || (precedance(operator.peek()) == precedance(element) && precedance(operator.peek()) != 3)) //Or precedence is equal and not ^
+                        && !operator.peek().equals("(")) { // And operator is not "("
+
+                    output.addLast(operator.pop()); //Add operators to output
+
+                }
+
+                operator.push(element);
+
+            }
+
+            else if (element.equals("(")) {
+                operator.push(element);
+            }
+
+            else if (element.equals(")")) {
+                try {
+
+                    while (!operator.peek().equals("("))
+                        output.addLast(operator.pop()); //Add operators to output
+
+                    if (operator.peek().equals("("))
+                        operator.pop();
+
+                } catch (EmptyStackException e1) {
+                    System.out.println("Error: Mismatched Parentheses");
+                }
             }
         }
 
+        while (!operator.isEmpty()) //If there are operators left over after tokens have been looked through
+            output.addLast(operator.pop()); //Add them all into output queue
 
     }
+
+
 }
